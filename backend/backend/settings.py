8da0872b9209db5bd1dfcc031c048a83a63adde6
sahal <sahalshalu830@gmail.com>
2024-04-07 10:12:08 +0530

    database changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,10 +92,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": os.environ.get('DATABASE_NAME'),
+       "USER": os.environ.get('DATABASE_USER'),
+       "PASSWORD": os.environ.get('DATABASE_PASSWORD'),
+       "HOST": os.environ.get('DATABASE_HOST'),
+       "PORT": os.environ.get('DATABASE_PORT'),
+   }
 }
 
 
